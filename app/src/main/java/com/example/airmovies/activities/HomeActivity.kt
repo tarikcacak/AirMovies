394bdad7ecaf64cc53f6382fe91ec0a89024aa26
Tarik Čačković <113112797+tarikcacak@users.noreply.github.com>
2023-10-02 16:23:22 +0200

    Started developing home fragment

--- app/src/main/java/com/example/airmovies/activities/HomeActivity.kt
+++ app/src/main/java/com/example/airmovies/activities/HomeActivity.kt
@@ -2,11 +2,19 @@ package com.example.airmovies.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.airmovies.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.btmNavMenu)
+        val navController = findNavController(R.id.fragmentContainer)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
